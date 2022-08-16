cc5d8b327314c8b87120004871b07782a7471a6b
duda1404 <dudinha140405@gmail.com>
2022-08-16 18:22:17 -0300

    Preenchi o dat_add_artista e coloquei o order by na  página artistas  (não funciona)
    o dat_add_artista é o atributo que pega a data atual de adição do artista no banco de dados, quando ocorre o insert em PHP (tá no cadastro de bandas normal e no cadastro de bandas admin). A data funciona, mas o order by na página de artistas, e por algum motivo não funciona

--- artistas.php
+++ artistas.php
@@ -18,7 +18,7 @@
 no banco. Enquanto houver resultado da consulta no MYSQL, executará o loop que preenche as informações*/ 
 				$image_query = mysqli_query($connect,"select artista.id_artista, artista.nome_artista, 
 				artista.dsc_artista, artista.link_play, foto_artista.photo_artista from artista inner join
-				foto_artista on artista.id_artista = foto_artista.FK_ARTISTA_id_artista");
+				foto_artista on artista.id_artista = foto_artista.FK_ARTISTA_id_artista order by artista.dat_add_artista");
 				while($rows = mysqli_fetch_array($image_query)){
 					$id_artista = $rows['id_artista'];
 					$nome_artista = $rows['nome_artista'];
