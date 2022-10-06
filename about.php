43ff7a3ca8762b7da7e1564835c930f31ba70706
Walentim <108528046+Walentim@users.noreply.github.com>
2022-10-06 15:21:03 -0300

    página SOBRE finalizada

--- about.php
+++ about.php
@@ -6,11 +6,34 @@
 
 <section class="about">
         <div class="main-about">
-            <img src="./img/rockxaba_icon.png">
+            <img class="rockxabaicon" src="./img/rockxaba_icon.png">
             <div class="about-text">
                 <h2>WHO THE F*CK ARE <span> RockXaba</span>?</h2>
                 <p> O <span>RockXaba</span> é um projeto que nasceu para apoiar o artista independente local, eventos alternativos e seu público da grande VIX. Aqui você vai achar artistas que são a sua cara, e claro, poder comprar aquela blusa da sua banda favorita da cena, além de saber quando e onde a galera  vai tocar e ainda poder dar seu feedback do evento. E pra você banda ou artista, seja nosso camarada! não há nenhum tipo de cobrança dos artistas ou dos usuários para usarem a plataforma, afinal esse lugar é NOSSO!
  </p>
+ <h2>Visite nossas <span>Redes Sociais</span></h2>
+                <ul>
+                    <li>
+                        <a href="" title="instagram">
+                            <img src="./img/instagram.png" width="80px" height="80px" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="" title="facebook">
+                            <img src="./img/facebook.png" width="80px" height="80px" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="" title="whatsapp">
+                            <img src="./img/whatsapp.png" width="80px" height="80px" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="" title="twitter">
+                            <img src="./img/twitter.png" width="80px" height="80px" />
+                        </a>
+                    </li>
+                </ul>
             </div>
         </div>
     </section>
