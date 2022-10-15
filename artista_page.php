9506c98686fe22d957f15bc62317e88e2c8b7f43
Gileardy <84356420+gileardy@users.noreply.github.com>
2022-10-15 13:12:00 -0300

    Terminei impressão de comentários e respostas

--- artista_page.php
+++ artista_page.php
@@ -44,101 +44,137 @@ no banco. */
                     </label>
                 </div>
 
+                
                 <div id="comentarios-caixa-artista-evento" style=" background: rgba(1,37,28,0.5); backdrop-filter: blur(2px)">
                     <div class="sessao-comentarios" id="sessao-comentarios1">
+                        
+                    <?php    $resultadon = pg_query($connect, "SELECT usuario.nome_user, usuario.photo_user, 
+                                comentario_artista.dsc_coment, comentario_artista.date_coment, comentario_artista.id_coment
+                                 FROM usuario INNER JOIN comentario_artista ON usuario.id_user = comentario_artista.FK_USUARIO_id_user 
+                                 INNER JOIN artista ON artista.id_artista = comentario_artista.FK_ARTISTA_id_artista
+                                 WHERE FK_ARTISTA_id_artista = $id and reply_of is null");
+
+
+                                while ($rows = pg_fetch_array($resultadon)) {
+                                    $id_coment = $rows['id_coment'];
+                                    $nome_userr = $rows['nome_user'];
+                                    $photo_user = $rows['photo_user'];
+                                    $coment = $rows['dsc_coment'];
+                                    $date_coment = $rows['date_coment'];
+                                ?>
+
                         <div class="caixa-comentario">
                             <div class="comentarios-top">
                                 <div class="foto-perfil">
-                                    <img class="mr-3 rounded-circle" id="foto-perfil-comentario" src="img/rex_orange.jpg">
+                                    <img class="mr-3 rounded-circle" id="foto-perfil-comentario" src="uploads/<?php echo $photo_user; ?>">
                                 </div>
                                 <div class="foto-nome-data-horario">
-                                    <h5> Walentim23_</h5>
-                                    <span> 22/10/2022 14:00 </span>
+                                    <h5> <?php echo $nome_userr; ?>_</h5>
+                                    <span> <?php echo $date_coment; ?> </span>
                                 </div>
                             </div>
-                            <p id="texto-notificacao"> Uma banda que você segue adicionou uma nova imagem! </p>
+                            <p id="texto-notificacao"> <?php echo $coment; ?> </p>
                             <div class="curtir-comentario">
                                 <button class="ver-respostas-responder-curtir" id="ver-respostas" onclick="minhaFuncao()"> Ver respostas(54)</button>
                                 <button class="ver-respostas-responder-curtir" id="responder" onclick="responderComentariosessao1()"> Responder </button>
                                 <button class="ver-respostas-responder-curtir" id="curtir"><img id="imagem-curtida" src="img/guitar-pick.svg" alt="Botão de curtida"> 145</button>
                             </div>
                         </div>
-                        <div class="caixa-comentario">
-                            <div class="comentarios-top">
-                                <div class="foto-perfil">
-                                    <img class="mr-3 rounded-circle" id="foto-perfil-comentario" src="img/rex_orange.jpg">
-                                </div>
-                                <div class="foto-nome-data-horario">
-                                    <h5> Walentim23_</h5>
-                                    <span> 22/10/2022 14:00 </span>
-                                </div>
-                            </div>
-                            <p id="texto-notificacao"> Uma banda que você segue adicionou uma nova imagem! </p>
-                            <div class="curtir-comentario">
-                                <button class="ver-respostas-responder-curtir" id="ver-respostas" onclick="minhaFuncao()"> Ver respostas(54)</button>
-                                <button class="ver-respostas-responder-curtir" id="responder" onclick="responderComentariosessao1()"> Responder </button>
-                                <button class="ver-respostas-responder-curtir" id="curtir"><img id="imagem-curtida" src="img/guitar-pick.svg" alt="Botão de curtida"> 145</button>
-                            </div>
-                        </div>
+                        
+                       
+                        <?php
+                                            }
+                                            ?>
                     </div>
+                    
+                 
+
                     <div class="sessao-comentarios" id="sessao-comentarios2" style="display:none">
+                   
                         <div class="caixa-comentario" id="comentario-principal-aba-respostas">
+
                             <div class="comentarios-top">
                                 <div class="foto-perfil">
-                                    <img class="mr-3 rounded-circle" id="foto-perfil-comentario" src="img/rex_orange.jpg">
+                                    <img class="mr-3 rounded-circle" id="foto-perfil-comentario" src="uploads/<?php echo $photo_user; ?>">
                                 </div>
                                 <div class="foto-nome-data-horario">
-                                    <h5> Walentim23_</h5>
-                                    <span> 22/10/2022 14:00 </span>
+                                    <h5> <?php echo $nome_userr; ?>_</h5>
+                                    <span> <?php echo $date_coment; ?> </span>
                                     <button class="fechar-aba-respostas" onclick="funcaoFechar()"><img id="icone-fechar-aba" src="img/close-outline.svg" alt="Fechar aba de comentários"></button>
                                 </div>
                             </div>
-                            <p id="texto-notificacao"> Uma banda que você segue adicionou uma nova imagem! </p>
+                            <p id="texto-notificacao"> <?php echo $coment; ?> </p>
                             <div class="curtir-comentario">
                                 <button class="ver-respostas-responder-curtir" id="responder" onclick="responderComentario()" > Responder </button>
                                 <button class="ver-respostas-responder-curtir" id="curtir"><img id="imagem-curtida" src="img/guitar-pick.svg" alt="Botão de curtida"> 145</button>
                             </div>
                         </div>
+                       
                         <div id="respostas" class="sessao-comentarios">
+                        <?php
+                                            $resultado_reply = pg_query($connect, "WITH RECURSIVE tree AS 
+                                            ( select
+            comentario_artista.dsc_coment, comentario_artista.date_coment, comentario_artista.id_coment, comentario_artista.reply_of, 
+            lpad(id_coment::text, 2, '0') as path, usuario.nome_user, usuario.photo_user   
+            FROM usuario INNER JOIN comentario_artista ON usuario.id_user = comentario_artista.FK_USUARIO_id_user 
+            
+            INNER JOIN artista ON artista.id_artista = comentario_artista.FK_ARTISTA_id_artista where id_artista = $id and reply_of is null
+           
+           union all 
+           
+           select 
+comentario_artista.dsc_coment, comentario_artista.date_coment, comentario_artista.id_coment, comentario_artista.reply_of,
+tree.path || '/' || comentario_artista.id_coment::text as path, usuario.nome_user, usuario.photo_user
+           
+            FROM usuario INNER JOIN comentario_artista ON usuario.id_user = comentario_artista.FK_USUARIO_id_user 
+
+            INNER JOIN artista ON artista.id_artista = comentario_artista.FK_ARTISTA_id_artista, tree 
+
+             WHERE comentario_artista.reply_of = tree.id_coment
+           )
+           SELECT * FROM tree where path like '$id_coment%' and reply_of is not null");
+
+
+           while ($reply_array = pg_fetch_array($resultado_reply)) {
+               $id_reply = $reply_array['id_coment'];
+               $reply = $reply_array['dsc_coment'];
+               $nome_user_reply = $reply_array['nome_user'];
+               $data_reply = $reply_array['date_coment'];
+               $photo_reply = $reply_array['photo_user'];
+
+           ?>
                             <div class="caixa-comentario">
                                 <div class=" comentarios-top">
                                 <div class="foto-perfil">
-                                    <img class="mr-3 rounded-circle" id="foto-perfil-comentario" src="img/rex_orange.jpg">
+                                    <img class="mr-3 rounded-circle" id="foto-perfil-comentario" src="uploads/<?php echo $photo_reply; ?>">
                                 </div>
                                 <div class="foto-nome-data-horario">
-                                    <h5> Walentim23_</h5>
-                                    <span> 22/10/2022 14:00 </span>
+                                    <h5> <?php echo $nome_user_reply; ?>_</h5>
+                                    <span> <?php echo $data_reply; ?> </span>
 
                                 </div>
                             </div>
-                            <p id="texto-notificacao"> Tô respondendo </p>
+                            <p id="texto-notificacao"> <?php echo $reply; ?> </p>
                             <div class="curtir-comentario">
                                 <button class="ver-respostas-responder-curtir" id="responder" onclick="responderComentario()"> Responder </button>
                                 <button class="ver-respostas-responder-curtir" id="curtir"><img id="imagem-curtida" src="img/guitar-pick.svg" alt="Botão de curtida"> 145</button>
                             </div>
                         </div>
-                        <div class="caixa-comentario">
-                            <div class="comentarios-top">
-                                <div class="foto-perfil">
-                                    <img class="mr-3 rounded-circle" id="foto-perfil-comentario" src="img/rex_orange.jpg">
-                                </div>
-                                <div class="foto-nome-data-horario">
-                                    <h5> Walentim23_</h5>
-                                    <span> 22/10/2022 14:00 </span>
-                                </div>
-                            </div>
-                            <p id="texto-notificacao"> AA </p>
-                            <div class="curtir-comentario">
-                                <button class="ver-respostas-responder-curtir" id="responder" onclick="responderComentario()"> Responder </button>
-                                <button class="ver-respostas-responder-curtir" id="curtir"><img id="imagem-curtida" src="img/guitar-pick.svg" alt="Botão de curtida"> 145</button>
-                            </div>
-                        </div>
+
+                        
+                        <?php
+                                            }
+                                            ?>
                     </div>
+                   
                 </div>
+
                 <div class="caixa-enviar-comentario" id="caixa-enviar">
                     <textarea id="text-area" class="input-comentario" placeholder="digite aqui..."></textarea>
                 </div>
+                
             </div>
+            
 
             <script>
                 function minhaFuncao() {
